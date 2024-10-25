b74b13aa412e37e297af169f6741bbc9e1f59608
mumu45a679065 <151854987+mumu45a679065@users.noreply.github.com>
2024-10-25 16:27:40 +0000

    container config done

---
+++ 01-basic/02-datatype.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all Js code as newer version of Js
+
+// alert(3+3) // in node js not allowed to use only in browser
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standAlone value
+// undefined => not defined yet
+// symbol => unique
+
+// object
+console.log(typeof undefined)
+console.log(typeof null)
