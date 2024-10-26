54bd213f4de2e80577346d77f575a65d964f4efa
mumu45a679065 <151854987+mumu45a679065@users.noreply.github.com>
2024-10-26 12:53:28 +0000

    updated file

--- 01-basic/03-conversionOperation.js
+++ 01-basic/03-conversionOperation.js
@@ -1,9 +1,9 @@
 let score = 'abc'
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -11,7 +11,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = "a"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -19,5 +19,48 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ******************* Operations *********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 3)
+// console.log(2 / 3)
+// console.log(2 % 3)
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(2 + 1 + "1")
+
+// console.log(true) // true
+// console.log(+true) // 1
+// // console.log(true+) // not allowed
+// console.log(+"") // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+// ++gameCounter;
+// console.log(gameCounter);
+
+// let a = 100
+// console.log(a++);
+// console.log(++a);
+
